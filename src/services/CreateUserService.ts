c9bd55608a6f080949a6fca4dc1491c76e8f8a0b
Geovani José <geovani-lol@hotmail.com>
2020-09-23 00:04:38 -0300

    Implement CreateUserService service

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,40 @@
+import { getRepository } from 'typeorm'
+import { hash } from 'bcryptjs'
+
+import User from '../models/User'
+
+interface Request {
+  name: string
+  email: string
+  password: string
+  age: number
+}
+
+class CreateUserService {
+  public async execute({ name, email, password, age }: Request): Promise<User> {
+    const usersRepository = getRepository(User)
+
+    const checkUserExists = await usersRepository.findOne({
+      where: { email }
+    })
+
+    if (checkUserExists) {
+      throw new Error('Email address already used.')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+      age
+    })
+
+    await usersRepository.save(user)
+
+    return user
+  }
+}
+
+export default CreateUserService
