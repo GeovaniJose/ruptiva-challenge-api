44c9214673b0c012782c120068aac2698bef50f4
Geovani José <geovani-lol@hotmail.com>
2020-09-23 00:05:11 -0300

    Create users route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express'
 
+import usersRouter from './users.routes'
+
 const routes = Router()
+
+routes.use('/users', usersRouter)
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello World' })
