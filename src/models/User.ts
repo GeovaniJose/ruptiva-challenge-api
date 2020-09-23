cb6320f93f2293909e5cf8dda2ff7b5cf5717564
Geovani José <geovani-lol@hotmail.com>
2020-09-22 22:07:07 -0300

    Implement User model

---
+++ src/models/User.ts
@@ -0,0 +1,36 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn
+} from 'typeorm'
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column('varchar')
+  name: string
+
+  @Column('varchar')
+  email: string
+
+  @Column('varchar')
+  password: string
+
+  @Column('integer')
+  age: number
+
+  @Column('varchar')
+  avatar: string
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+}
+
+export default User
