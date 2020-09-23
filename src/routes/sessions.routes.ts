cc5523613299efd7fe3e845deb43736cfa96acda
Geovani José <geovani-lol@hotmail.com>
2020-09-23 00:56:46 -0300

    Generate JWT token

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -1,4 +1,4 @@
-import { response, Router } from 'express'
+import { Router } from 'express'
 
 import AuthenticateUserService from '../services/AuthenticateUserService'
 
@@ -10,14 +10,14 @@ sessionsRouter.post('/', async (request, response) => {
 
     const authenticateUser = new AuthenticateUserService()
 
-    const { user } = await authenticateUser.execute({
+    const { user, token } = await authenticateUser.execute({
       email,
       password
     })
 
     delete user.password
 
-    return response.status(200).json({ user })
+    return response.status(200).json({ user, token })
   } catch (err) {
     return response.status(400).json({ error: err.message })
   }
