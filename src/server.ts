4fe1ad20341572a46a62e3dfbd48c703c7bd49a6
Geovani José <geovani-lol@hotmail.com>
2020-09-22 23:16:02 -0300

    Create routes

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import express from 'express'
+import cors from 'cors'
+
+import routes from './routes'
 
 import './database'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333...')
