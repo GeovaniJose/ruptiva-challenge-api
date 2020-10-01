c47e9276fadb7375696dbb9570d4c9026d5bacfc
Geovani José <geovani-lol@hotmail.com>
2020-10-01 12:15:10 -0300

    Implement put user route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer'
 
 import CreateUserService from '../services/CreateUserService'
 import UpdateUserAvatarService from '../services/UpdateUserAvatarService'
+import UpdateUserService from '../services/UpdateUserService'
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 import uploadConfig from '../config/upload'
@@ -19,6 +20,22 @@ usersRouter.post('/', async (request, response) => {
     name,
     email,
     password,
+    age
+  })
+
+  delete user.password
+
+  return response.status(200).json(user)
+})
+
+usersRouter.put('/', ensureAuthenticated, async (request, response) => {
+  const { name, age } = request.body
+
+  const updateUser = new UpdateUserService()
+
+  const user = await updateUser.execute({
+    user_id: request.user.id,
+    name,
     age
   })
 
